5ea687a4623b1fc8611d83b4d7aee97633ffbbaf
nextterminal <nextterminal@tutamail.com>
2025-01-20 16:11:40 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,23 +1,40 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST["name"]);
     $email = htmlspecialchars($_POST["email"]);
     $message = htmlspecialchars($_POST["message"]);
 
-    $to = "info@digitalfingers.shop";
-    $subject = "New Contact Form Message from Digital Fingers";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $mail = new PHPMailer(true);
 
-    $body = "You have received a new message from your website contact form.\n\n";
-    $body .= "Name: $name\n";
-    $body .= "Email: $email\n";
-    $body .= "Message:\n$message\n";
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'mail.privateemail.com'; // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'info@digitalfingers.shop'; // SMTP username
+        $mail->Password   = 'Labello1.'; // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
 
-    if (mail($to, $subject, $body, $headers)) {
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('info@digitalfingers.shop');
+
+        // Content
+        $mail->isHTML(false);
+        $mail->Subject = 'New Contact Form Message from Digital Fingers';
+        $mail->Body    = "You have received a new message from your website contact form.\n\nName: $name\nEmail: $email\nMessage:\n$message\n";
+
+        $mail->send();
         echo "<script>alert('Message sent successfully! We will get back to you soon.'); window.location.href='index.html';</script>";
-    } else {
+    } catch (Exception $e) {
         echo "<script>alert('Message sending failed. Please try again later.'); window.location.href='index.html';</script>";
     }
 }
